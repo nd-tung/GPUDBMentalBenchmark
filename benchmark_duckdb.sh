e1d02ebefe9e8e9418f7d354afc8ed54e4e8e6e7
nd-tung <duong-tung.nguyen@stud.uni-bamberg.de>
2025-10-25 13:49:36 +0200

    DuckDB: use standard TPC-H Q13 SQL

--- benchmark_duckdb.sh
+++ benchmark_duckdb.sh
@@ -438,11 +438,13 @@ run_benchmarks() {
             COUNT(*) AS custdist
         FROM (
             SELECT
+                c.c_custkey,
                 COUNT(o.o_orderkey) AS c_count
-            FROM orders o
-            RIGHT OUTER JOIN (SELECT DISTINCT l_orderkey FROM lineitem LIMIT 1000) c ON o.o_orderkey = c.l_orderkey
-            WHERE o.o_comment NOT LIKE '%special%requests%'
-            GROUP BY c.l_orderkey
+            FROM customer c
+            LEFT OUTER JOIN orders o
+              ON c.c_custkey = o.o_custkey
+             AND o.o_comment NOT LIKE '%special%requests%'
+            GROUP BY c.c_custkey
         ) AS c_orders
         GROUP BY c_count
         ORDER BY custdist DESC, c_count DESC;" \
